722173f7a4f68220028dfafdc4bec234429461ec
Eclipse <simernin.mat@gmail.com>
2023-09-20 14:57:24 +0300

    first commit

---
+++ Лекция№2.py
@@ -0,0 +1,14 @@
+# x = 10
+# y = 5
+# max_value = x if x > y else y
+# print(max_value)
+
+# count = 0
+# while count < 100:
+#     print("Счетчик", count)
+#     count += 1
+
+
+# fruits = ['яблоко', 'груша', 'банан']
+# for fruit in fruits:
+#     print(fruit)
---
+++ Лекция№3.py
@@ -0,0 +1,17 @@
+# def add(a, b):
+#     result = a+b
+#     return result
+
+# x = add(2, 5)
+# print(x)
+
+
+def calculate(a, b):
+    resultSum = a + b
+    resultMin = a - b
+    return resultSum, resultMin
+
+x, y = calculate(b = 2, a = 5)
+print(x, y)
+
+
